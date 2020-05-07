768d09f1cec7ddb3b94bf153b6d137cc67b6c340
Denys Pavlov <me@denys.me>
2020-05-07 16:30:12 -0400

    feat: add some packages, a regular user

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,4 +1,12 @@
 { config, modulesPath, pkgs, lib, ... }:
+let
+  fetchLines = url:
+    lib.pipe url [
+      builtins.fetchurl
+      lib.fileContents
+      (lib.splitString "\n")
+    ];
+in
 {
   imports = [
     "${toString modulesPath}/virtualisation/digital-ocean-image.nix"
@@ -6,9 +14,22 @@
     ./wireguard.nix
   ];
 
-  environment.systemPackages = [ pkgs.mosh ];
+  environment.systemPackages = [
+    pkgs.mosh
+    pkgs.byobu
+    pkgs.tmux
+    pkgs.direnv
+    pkgs.vim
+  ];
 
   users.users.root.openssh.authorizedKeys.keys =
-    lib.splitString "\n"
-      (lib.removeSuffix "\n" (builtins.readFile (builtins.fetchurl "https://github.com/meatcar.keys")));
+    fetchLines "https://github.com/meatcar.keys";
+
+  users.users.meatcar = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$6$TW7fMuG2cMYbWDC$p0t7uFxePu/U.Lp8MUp0tgoJZh.EL7MkC3SG5jsNCIXkh2S.LA8wxnUEZpG4Mnvk/C3WOMKz35YXCC0XDkZWm/";
+    openssh.authorizedKeys.keys =
+      fetchLines "https://github.com/meatcar.keys";
+  };
 }
