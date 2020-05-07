ae672a7ee3837698864415a4f2c5dcea992ef744
Denys Pavlov <me@denys.me>
2020-05-07 16:29:16 -0400

    feat: enable swap, nix cleanup/autoupgrade, mosh

--- nix/base.nix
+++ nix/base.nix
@@ -1,5 +1,18 @@
 { config, lib, ... }:
 {
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 2048;
+    }
+  ];
+
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-20.03";
+  nix.gc.automatic = true;
+  nix.autoOptimiseStore = true;
+
   services.sshd.enable = true;
+  programs.mosh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
 }
