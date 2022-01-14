61a3f068c872c735fc0490ab81e03cd5a5e2d634
Denys Pavlov <me@denys.me>
2022-01-14 21:12:00 +0000

    fix: make config pure

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,15 @@
 { config, modulesPath, pkgs, lib, ... }:
 let
-  fetchLines = url:
-    lib.pipe url [ builtins.fetchurl lib.fileContents (lib.splitString "\n") ];
+  sshKeys = lib.pipe
+    {
+      url = "https://github.com/${config.mine.githubKeyUser}.keys";
+      sha256 = "sha256:1g214y4ibdnxavsbrhds1hwxz30lff808nh2kzw3wxwkb8f1z1yd";
+    }
+    [
+      builtins.fetchurl
+      builtins.readFile
+      (lib.splitString "\n")
+    ];
 in
 {
   imports = [
@@ -57,16 +65,14 @@ in
     environment.systemPackages =
       [ pkgs.mosh pkgs.byobu pkgs.tmux pkgs.direnv pkgs.vim pkgs.git ];
 
-    users.users.root.openssh.authorizedKeys.keys =
-      fetchLines "https://github.com/${config.mine.githubKeyUser}.keys";
+    users.users.root.openssh.authorizedKeys.keys = sshKeys;
 
     users.users.meatcar = {
       isNormalUser = true;
       extraGroups = [ "wheel" "docker" "nginx" ];
       hashedPassword =
         "$6$TW7fMuG2cMYbWDC$p0t7uFxePu/U.Lp8MUp0tgoJZh.EL7MkC3SG5jsNCIXkh2S.LA8wxnUEZpG4Mnvk/C3WOMKz35YXCC0XDkZWm/";
-      openssh.authorizedKeys.keys =
-        fetchLines "https://github.com/${config.mine.githubKeyUser}.keys";
+      openssh.authorizedKeys.keys = sshKeys;
     };
   };
 }
