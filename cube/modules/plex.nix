848bd2a467cb252006276c0476e894c36d08ce50
Denys Pavlov <me@denys.me>
2022-01-21 07:25:11 +0000

    feat(cube): improve nginx plex performance

--- cube/modules/plex.nix
+++ cube/modules/plex.nix
@@ -1,4 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
+let
+  # copy a store path to a have a new name.
+  # Used to rename flake input paths from `<hash>-source`, since
+  # services.plex.managePlugins calls `basename` on the path, and plex expects
+  # the  directory name to be formated like `<name>.bundle`
+  renamePath = path: name: pkgs.runCommand name { inherit path name; } ''
+    mkdir -p $out
+    cp -r $path/* $out
+  '';
+in
 {
   config = {
     systemd.tmpfiles.rules = [
@@ -10,12 +20,24 @@
     services.plex = {
       enable = true;
       openFirewall = true;
+      managePlugins = true;
+      extraPlugins = [ (renamePath inputs.plex-subzero "Sub-Zero.bundle") ];
     };
 
-    services.nginx.virtualHosts."plex.${config.fqdn}" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/".proxyPass = "http://127.0.0.1:32400";
+    services.nginx = {
+      clientMaxBodySize = "100M";
+      virtualHosts."plex.${config.fqdn}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:32400";
+          proxyWebsockets = true;
+        };
+        extraConfig = ''
+          proxy_buffering off;
+          send_timeout 100m;
+        '';
+      };
     };
   };
 }
