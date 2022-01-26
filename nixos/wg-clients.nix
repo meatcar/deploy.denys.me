d7ddb69ada50927d4fa6345c7749921841f78122
Denys Pavlov <me@denys.me>
2022-01-26 05:22:14 +0000

    refactor: pull build-time secrets from environment

---
+++ nixos/wg-clients.nix
@@ -0,0 +1,17 @@
+[
+  {
+    # laptop
+    publicKey = "MOX+q35dIXuUNzZlEReZr7NLsuARVUoPgBl78SAQdU4=";
+    allowedIPs = [ "10.100.0.2/32" ];
+  }
+  {
+    # phone
+    publicKey = "CnmO5cGmqu6kAMMZwhdCi85oln+PqVfd/3N4OlGrilI=";
+    allowedIPs = [ "10.100.0.3/32" ];
+  }
+  {
+    # cube.denys.me
+    publicKey = "9z01LiXRxCMWQrCi7vh88kASdYvexrpcx9xbktX7elQ=";
+    allowedIPs = [ "10.100.0.4/32" ];
+  }
+]
