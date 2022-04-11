b4570ac3c67f189f41f9e6146caaf2b1391dd431
Denys Pavlov <me@denys.me>
2022-04-11 20:48:35 +0000

    fix: fetch cloudflare_zone id

--- dns.tf
+++ dns.tf
@@ -1,5 +1,9 @@
+resource "cloudflare_zone" "main" {
+  zone = var.cloudflare_domain
+}
+
 resource "cloudflare_record" "A-www" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = cloudflare_zone.main.id
   type    = "A"
   name    = var.hostname
   value   = digitalocean_droplet.www.ipv4_address
@@ -7,7 +11,7 @@ resource "cloudflare_record" "A-www" {
 }
 
 resource "cloudflare_record" "CNAME-www-wildcard" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = cloudflare_zone.main.id
   type    = "CNAME"
   name    = "*.${cloudflare_record.A-www.name}"
   value   = cloudflare_record.A-www.name
