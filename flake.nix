c62ad1de0731184dda8d683453d77d7131ccc286
Denys Pavlov <me@denys.me>
2022-06-02 19:26:27 +0000

    chore: clean up deploy script

--- flake.nix
+++ flake.nix
@@ -9,52 +9,33 @@
 
   outputs = { self, ... }@inputs:
     let
-      nixpkgsConfig = {
-        config = { allowUnfree = true; };
-      };
+      nixpkgsConfig = { config = { allowUnfree = true; }; };
       specialArgs = { inherit inputs; };
     in
-    (inputs.flake-utils.lib.eachDefaultSystem
+    inputs.flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import inputs.nixpkgs (nixpkgsConfig // { inherit system; });
-          nix = pkgs.nixFlakes;
           scripts = [
-            (pkgs.writeShellScriptBin "push-and-rebuild" ''
-              IP=$(${pkgs.terraform}/bin/terraform output --raw ip)
+            (pkgs.writeShellScriptBin "deploy" ''
+              IP=$(cd terraform && ${pkgs.terraform}/bin/terraform output --raw ip)
               REMOTE_USER=meatcar
-              HOST=$REMOTE_USER@$IP
-              # echo "Pushing to $HOST"
-              # ${pkgs.rsync}/bin/rsync -avz nixos "$HOST:/etc"
-              if [ -n "$SUDO" ]; then
-                echo "** sudo password for $HOST will be required..."
-              fi
-              nixos-rebuild switch --target-host $HOST --flake .#default --use-remote-sudo --impure --build-host $HOST --use-substitutes
-            ''
-            )
-            (pkgs.writeShellScriptBin "dev-watch" ''
-              while true; do
-              ${pkgs.fd}/bin/fd nixos | \
-              ${pkgs.entr}/bin/entr -dc push-and-rebuild
-              done
-            ''
-            )
+              REMOTE_HOST=$REMOTE_USER@$IP
+              nixos-rebuild "$@" \
+                --flake .#vps \
+                --target-host "$REMOTE_HOST" \
+                --use-remote-sudo \
+                --build-host "$REMOTE_HOST" \
+                --use-substitutes
+            '')
           ];
         in
         {
           devShells.default = pkgs.mkShell rec {
             name = "deploy.denys.me";
-            NIX_PATH = builtins.concatStringsSep ":" [
-              "nixpkgs=${inputs.nixpkgs}"
-            ];
-            shellHook = ''
-              export NIX_SSHOPTS=-t
-            '';
-            buildInputs = with pkgs; scripts ++ [
+            buildInputs = scripts ++ (with pkgs; [
               inputs.agenix.defaultPackage.${system}
-              packer
-              nix
-              (pkgs.nixos-rebuild.override { inherit nix; })
+
               (terraform.withPlugins (p: [
                 p.local
                 p.external
@@ -67,27 +48,27 @@
               awscli
               wireguard-tools
               jq
-            ];
+            ]);
           };
-        })
-    // {
-      nixosConfigurations.default =
-        inputs.nixpkgs.lib.nixosSystem
+        }) // {
+      nixosConfigurations.vps = inputs.nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        system = "x86_64-linux";
+        modules = [
           {
-            inherit specialArgs;
-            system = "x86_64-linux";
-            modules = [
-              { nixpkgs = nixpkgsConfig; }
-              inputs.agenix.nixosModules.age
-              {
-                age.secrets = {
-                  wg-priv-key.file = ./secrets/wg-priv-key.age;
-                  restic-password.file = ./secrets/restic-password.age;
-                  restic-env.file = ./secrets/restic-env.age;
-                };
-              }
-              ./nixos/configuration.nix
-            ];
-          };
-    });
+            nixpkgs = nixpkgsConfig;
+            system.stateVersion = "22.05";
+          }
+          inputs.agenix.nixosModules.age
+          {
+            age.secrets = {
+              wg-priv-key.file = ./secrets/wg-priv-key.age;
+              restic-password.file = ./secrets/restic-password.age;
+              restic-env.file = ./secrets/restic-env.age;
+            };
+          }
+          ./nixos/configuration.nix
+        ];
+      };
+    };
 }
