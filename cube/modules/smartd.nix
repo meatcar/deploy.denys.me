308b4a4319b0d70ecc05c40cd0b0f906b3bd6605
Denys Pavlov <me@denys.me>
2023-03-01 13:29:08 -0500

    feat(cube): smartd should monitor removable drives

--- cube/modules/smartd.nix
+++ cube/modules/smartd.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = [ pkgs.smartmontools ];
   services.smartd = {
     enable = true;
-    defaults.monitored = "-a -n standby,24 -o on -s (S/../.././02|L/../../7/04)";
+    defaults.monitored = "-a -n standby,24 -o on -s (S/../.././02|L/../../7/04) -d removable";
     notifications = {
       test = true;
       wall.enable = false;
