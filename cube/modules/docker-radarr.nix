ef781c0db6677e0630e6d9c3eaaabb71f25f5483
Denys Pavlov <me@denys.me>
2023-07-27 14:47:46 -0400

    feat(cube): dockerize plex, radarr, sonarr, tautulli
    helps keep them up-to-date

---
+++ cube/modules/docker-radarr.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.services.radarr;
+  port = toString cfg.port;
+in
+{
+  options = {
+    services.radarr = {
+      port = lib.mkOption {
+        type = lib.types.int;
+        description = "radarr port to listen to locally";
+        default = 7878;
+      };
+    };
+  };
+
+  config = {
+    systemd.tmpfiles.rules = [
+      "d ${config.storagePath}/radarr 0755 - - - -"
+    ];
+
+    virtualisation.oci-containers.containers.radarr = {
+      image = "lscr.io/linuxserver/radarr";
+      ports = [ "${port}:7878" ];
+      volumes = [
+        "${config.persistPath}/radarr:/config"
+        "/data:/data"
+      ];
+      extraOptions = [ "--network=host" ];
+      environment = {
+        PUID = toString config.ids.uids.${config.storageUser};
+        PGID = toString config.ids.gids.${config.storageGroup};
+      };
+    };
+
+    services.nginx.virtualHosts."radarr.${config.fqdn}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/".proxyPass = "http://127.0.0.1:${port}";
+    };
+  };
+}
---
+++ cube/modules/docker-sonarr.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.services.sonarr;
+  port = toString cfg.port;
+in
+{
+  options = {
+    services.sonarr = {
+      port = lib.mkOption {
+        type = lib.types.int;
+        description = "sonarr port to listen to locally";
+        default = 8989;
+      };
+    };
+  };
+
+  config = {
+    systemd.tmpfiles.rules = [
+      "d ${config.storagePath}/sonarr 0755 - - - -"
+    ];
+
+    virtualisation.oci-containers.containers.sonarr = {
+      image = "lscr.io/linuxserver/sonarr";
+      ports = [ "${port}:8989" ];
+      volumes = [
+        "${config.persistPath}/sonarr:/config"
+        "/data:/data"
+      ];
+      extraOptions = [ "--network=host" ];
+      environment = {
+        PUID = toString config.ids.uids.${config.storageUser};
+        PGID = toString config.ids.gids.${config.storageGroup};
+      };
+    };
+
+    services.nginx.virtualHosts."sonarr.${config.fqdn}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/".proxyPass = "http://127.0.0.1:${port}";
+    };
+  };
+}
