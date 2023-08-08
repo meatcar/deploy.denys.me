ec9a1595bc4ce0816c5ddf0216f6906169d6e2fc
Denys Pavlov <me@denys.me>
2023-08-08 16:43:49 -0400

    fix(restic): set proper passwords

--- flake.nix
+++ flake.nix
@@ -88,6 +88,7 @@
                 wg-priv-key.file = ./secrets/wg-priv-key.age;
                 restic-password.file = ./secrets/restic-password.age;
                 restic-env.file = ./secrets/restic-env.age;
+                restic-repo.file = ./secrets/restic-repo.age;
               };
             }
             ./nixos/systems/vps.nix
