26cdef8ab73041426e46dd2477e484164556a02e
Denys Pavlov <me@denys.me>
2023-08-11 02:32:37 -0400

    feat: merge cube's flake.nix
    - rewrite deploy script to discriminate targets

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    wsdd = {
+      url = "github:christgau/wsdd";
+      flake = false;
+    };
+
     website = {
       url = "github:meatcar/denys.me";
       flake = false;
@@ -28,15 +33,33 @@
           pkgs = import inputs.nixpkgs (nixpkgsConfig // { inherit system; });
           scripts = [
             (pkgs.writeShellScriptBin "deploy" ''
-              IP=$(cd terraform && ${pkgs.terraform}/bin/terraform output --raw ip)
-              REMOTE_USER=meatcar
-              REMOTE_HOST=$REMOTE_USER@$IP
-              nixos-rebuild "$@" \
-                --flake .#vps \
+              FLAKE="$1"; shift 1
+              REMOTE_HOST=
+              case "$FLAKE" in
+                vps)
+                  REMOTE_HOST=$(cd terraform && ${pkgs.terraform}/bin/terraform output --raw ip)
+                  ;;
+                cube)
+                  REMOTE_HOST=10.100.0.4
+                  ;;
+                *)
+                  echo no such remote host "$FLAKE" >&2
+                  exit 1
+              esac
+
+              if ! ${pkgs.iputils}/bin/ping -c1 -W1 "$REMOTE_HOST"; then
+                echo "$0: no connection to $REMOTE_HOST" >&2
+                exit 1
+              fi
+
+              cmd=$(echo nixos-rebuild "$@" \
+                --flake .#"$FLAKE" \
                 --target-host "$REMOTE_HOST" \
-                --use-remote-sudo \
                 --build-host "$REMOTE_HOST" \
-                --use-substitutes
+                --use-remote-sudo \
+                --use-substitutes)
+              echo "$cmd"
+              $cmd
             '')
           ];
         in
@@ -110,6 +133,17 @@
             }
             inputs.nixos-hardware.nixosModules.raspberry-pi-4
             ./nixos/systems/rpi.nix
+          ];
+        };
+        cube = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs =
+            { inherit inputs; };
+          modules = [
+            inputs.agenix.nixosModules.default
+            ./cube/secrets/module.nix # agenix encrypted sensitive secrets
+            ./cube/modules/unencrypted-secrets.nix # less sensitive secrets that shouldn't be in git history
+            ./cube/configuration.nix
           ];
         };
       };
