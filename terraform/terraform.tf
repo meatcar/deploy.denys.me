680b709c358ecd7738f4daddfc19bc0f6339e30e
Denys Pavlov <me@denys.me>
2025-05-24 17:17:01 -0400

    fix(terraform): update lock, state locking

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-denys-me"
-    key            = "state"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock-denys-me"
+    bucket       = "terraform-state-denys-me"
+    key          = "state"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 
   required_providers {
