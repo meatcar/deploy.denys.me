90b885b012ac461f2bcffa41fb77480960c5605d
Denys Pavlov <me@denys.me>
2025-05-24 17:22:20 -0400

    fix: post-update samba tweak

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -3,18 +3,20 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+{
   services.samba = {
     enable = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      guest account = ${config.mine.storageUser}
-      map to guest = Bad Password
-      server min protocol = SMB2_10
-      client min protocol = SMB2
-      client max protocol = SMB3
-    '';
-    shares = {
+    smbd.enable = true;
+    nsswins = true;
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "guest account" = config.mine.storageUser;
+        "server min protocol" = "SMB2_10";
+        "client min protocol" = "SMB2";
+        "client max protocol" = "SMB3";
+      };
       data = {
         browseable = "yes";
         "guest ok" = "yes";
@@ -25,24 +27,37 @@
     };
   };
 
-  systemd.services.samba-wsdd = let
-    wsdd = inputs.wsdd;
-  in {
-    enable = true;
-    description = "Web Service Discovery Daemon";
-    documentation = ["https://github.com/christgau/wsdd"];
-    after = [
-      "network-online.target"
-      "samba-smbd.service"
-    ];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.python3}/bin/python3 ${wsdd}/src/wsdd.py --shortlog";
-      User = config.mine.storageUser;
-      Group = config.mine.storageGroup;
+  systemd.services.samba-wsdd =
+    let
+      wsdd = inputs.wsdd;
+    in
+    {
+      enable = true;
+      description = "Web Service Discovery Daemon";
+      documentation = [ "https://github.com/christgau/wsdd" ];
+      after = [
+        "network-online.target"
+        "samba-smbd.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.python3}/bin/python3 ${wsdd}/src/wsdd.py --shortlog";
+        User = config.mine.storageUser;
+        Group = config.mine.storageGroup;
+      };
     };
-  };
 
-  networking.firewall.allowedTCPPorts = [445 139] ++ [3702 5357];
-  networking.firewall.allowedUDPPorts = [137 138];
+  networking.firewall.allowedTCPPorts =
+    [
+      445
+      139
+    ]
+    ++ [
+      3702
+      5357
+    ];
+  networking.firewall.allowedUDPPorts = [
+    137
+    138
+  ];
 }
